1f0f0ff9835397ff37d59e24104569f75c6b4c12
only1-adams <muhammedadam4544@gmail.com>
2025-01-08 14:13:49 +0100

    auth

--- next.config.ts
+++ next.config.ts
@@ -15,7 +15,11 @@ const nextConfig: NextConfig = {
 			},
 		],
 	},
-	ignoreDuringBuilds: true,
+	eslint: {
+		// Warning: This allows production builds to successfully complete even if
+		// your project has ESLint errors.
+		ignoreDuringBuilds: true,
+	},
 };
 
 export default nextConfig;
